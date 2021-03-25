b323a9c0ee4611617e6f8247319ae1eccd9d0ae8
David Felipe Bautista Bernal <davidbautistab@usantotomas.edu.co>
2021-03-25 00:07:17 -0500

    Domino_piling y Bit++

---
+++ soluciones/Bit++.py
@@ -0,0 +1,10 @@
+#codigo BIT++ by:David Bautista 
+n=int(input(""))  
+x=0 .   
+for i in range(n):      
+  op=input("")          
+  if op[1]=="+":       
+    x+=1                
+  if op[1]=="-":    
+    x-=1             
+print(x) 
